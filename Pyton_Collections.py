7063e2ed9ebd883845a14853ab2418d61845e3f8
focodecided <chinaseeze@gmail.com>
2023-12-07 00:14:36 -0600

    Python Collections

---
+++ Pyton_Collections.py
@@ -0,0 +1,14 @@
+# Assign a list of ten string elements to a variable
+my_list = ["apple", "banana", "orange", "grape", "pear", "peach", "plum", "cherry", "mango", "strawberry"]
+
+# Print the fourth element of the list
+print("Fourth element:", my_list[3])  # Python uses 0-based indexing, so the fourth element is at index 3
+
+# Print the sixth through tenth element of the list
+print("Sixth through tenth elements:", my_list[5:])  # Slicing from index 5 to the end of the list
+
+# Change the value of the seventh element to "onion"
+my_list[6] = "onion"
+
+# Print the updated list
+print("Updated list:", my_list)
