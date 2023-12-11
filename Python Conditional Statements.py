2e098e1c5615d4838a555e73437647b5b6dfa0b0
focodecided <chinaseeze@gmail.com>
2023-12-10 18:56:22 -0600

    Python Conditional Statements

---
+++ Python Conditional Statements.py
@@ -0,0 +1,18 @@
+a = 10
+b = 20
+
+# If statement using a logical conditional
+if a < b:
+    print("a is less than b")
+elif a == b:
+    print("a is equal to b")
+else:
+    print("a is greater than b")
+
+# If statement with multiple conditions
+if a != b:
+    print("a is not equal to b")
+elif a >= b:
+    print("a is greater than or equal to b")
+else:
+    print("None of the conditions are met")
